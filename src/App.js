c91b96711dc56617f16b38e6bf383d768a7b3720
Ankit Kumar <anishkumar415263@gmail.com>
2024-07-28 18:43:05 +0530

    CRUD completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Create from './components/Create';
 import Read from './components/Read';
 import Toast from './components/Toast';
+import Update from './components/Update';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Create />} />
         <Route path='/read' element={<Read />} />
+        <Route path='/update' element={<Update />} />
       </Routes>
     </BrowserRouter>
   );
